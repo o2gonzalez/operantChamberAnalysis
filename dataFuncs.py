f49322b1ffeb8e4466985630eeffdfd12c0cf9c5
Oscar Christian Gonzalez <ocgonzal@stanford.edu>
2022-09-22 10:05:46 -0700

    added getRewardCounts function to return the percentage of trial where rewards were given

--- dataFuncs.py
+++ dataFuncs.py
@@ -3,10 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #--- Helper functions ---#
+# load data #
 def loadData(fileNames):
-    data = []
-    with open(fileNames) as f:
-        contents = f.readlines()
-        for lines in contents:
-            data.append(lines.split())
-    return data
+    dataList = []
+    for fileID in fileNames:
+        with open(fileID) as f:
+            data = []
+            contents = f.readlines()
+            for lines in contents:
+                data.append(lines.split())
+        dataList.append(data)
+    return dataList
+
+# get reward info #
+def getRewardCounts(dataList,names,dArrayStart=39,rewardNosePokePosition=9):
+    performance = np.zeros(shape=len(names))
+    for ii in range(len(names)):
+        numTrials = int(float(dataList[ii][36][1]))
+        rewardNosePokes = []
+        for jj in range(dArrayStart,dArrayStart+numTrials):
+            rewardNosePokes.append(float(dataList[ii][jj][rewardNosePokePosition]))
+        rewardNosePokes = np.array(rewardNosePokes)
+        nonPokes = np.where(rewardNosePokes==0)[0]
+        percentPokes = ((numTrials-nonPokes.shape[0])/numTrials)*100
+        print("Animal ID: ",names[ii],"| Percent of Trials with Nose Pokes: ",percentPokes,"%\n")
+        performance[ii] = percentPokes
+    return performance
--- mainAnalysis.py
+++ mainAnalysis.py
@@ -1,6 +1,5 @@
 ###--- LIBRARY IMPORTS ---###
-import re
-from this import d
+from importlib import reload
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,28 +7,10 @@ import dataFuncs
 
 #--- load data ---#
 fileNames = ['3rdDayRewardHabBL32.txt','3rdDayRewardHabBR22.txt','3rdDayRewardHabTL31.txt','3rdDayRewardHabTR24.txt']
-names = ['BL32','BR22','TL31','TR24']
+names = ['BL32','BR22','TL31','TR24'] # Animal IDs
+dataList = dataFuncs.loadData(fileNames) # creates list of complete datasets | dataList[animal][row][col]
+
+#--- get reward info per animal ---#
+rewardCounts = dataFuncs.getRewardCounts(dataList,names) # returns / prints the percent performance | (number of trials with reward / total number of trials) * 100
 
 
-data = dataFuncs.loadData(fileNames)
-rewardCounts = dataFuncs.getRewardCounts(data)
-
-
-for fileID in range(len(fileNames)):
-    with open(fileNames[fileID]) as f:
-        data = []
-        contents = f.readlines()
-        for lines in contents:
-            data.append(lines.split())
-
-    numTrials = int(float(data[36][1]))
-    rewardNosePokes = []
-    dArrayStart = 39
-    rewardNosePokePosition = 9
-    for ii in range(dArrayStart,dArrayStart+numTrials):
-        rewardNosePokes.append(float(data[ii][rewardNosePokePosition]))
-    rewardNosePokes = np.array(rewardNosePokes)
-
-    nonPokes = np.where(rewardNosePokes==0)[0]
-    percentPokes = ((numTrials-nonPokes.shape[0])/numTrials)*100
-    print("Animal ID: ",names[fileID],"| Percent of Trials with Nose Pokes: ",percentPokes,"%\n")
