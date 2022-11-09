9cd6b00ac9cc586f05d92e649d5f7964750ff1bb
Oscar Christian Gonzalez <ocgonzal@stanford.edu>
2022-11-09 15:39:55 -0800

    updated mainAnalysis and dataFuncs to include pInhibition function, and function to return percent correct/incorrect for specific tasks

--- dataFuncs.py
+++ dataFuncs.py
@@ -1,6 +1,7 @@
 ###--- LIBRARY IMPORTS ---###
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 #--- Helper functions ---#
 # load data #
@@ -15,6 +16,23 @@ def loadData(fileNames):
         dataList.append(data)
     return dataList
 
+# get percent GO correct #
+def getStats(dataList,names,correctPos,totalPos,incorrectPos=None):
+    percentCorrect = np.zeros(shape=len(names))
+    percentIncorrect = np.zeros(shape=len(names))
+    for ii in range(len(names)):
+        # correct trials
+        totalTrials = int(float(dataList[ii][35][totalPos]))
+        correct = int(float(dataList[ii][35][correctPos]))
+        percentCorrect[ii] = correct/totalTrials
+        # incorrect trials
+        if(incorrectPos != None):
+            incorrect = int(float(dataList[ii][35][incorrectPos]))
+            percentIncorrect[ii] = incorrect/totalTrials
+    return percentCorrect,percentIncorrect
+        
+
+#--- data functions ---# 
 # get reward info #
 def getRewardCounts(dataList,names,dArrayStart=39,rewardNosePokePosition=9):
     performance = np.zeros(shape=len(names))
@@ -29,3 +47,37 @@ def getRewardCounts(dataList,names,dArrayStart=39,rewardNosePokePosition=9):
         print("Animal ID: ",names[ii],"| Percent of Trials with Nose Pokes: ",percentPokes,"%\n")
         performance[ii] = percentPokes
     return performance
+
+def dataStats(dataList,names,statType):
+    if(statType=='Go'):
+        correctPos = 7
+        totalPos = 4
+        incorrectPos = 8
+    elif(statType=='NoGo'):
+        correctPos = 10
+        totalPos = 5
+        incorrectPos = 9
+    elif(statType=='PreCue'):
+        correctPos = 6
+        totalPos = 1
+        incorrectPos = None
+    percentCorrect,percentIncorrect = getStats(dataList,names,correctPos,totalPos,incorrectPos)
+    return percentCorrect,percentIncorrect
+
+# Inhibition control (P_inhibition) #
+def pInhibition(dataList,names):
+    pNoGo,tmp = dataStats(dataList,names,'NoGo')
+    tmp,pOmission = dataStats(dataList,names,'Go')
+    pInhibit = (pNoGo-pOmission)/(1-pOmission)
+    return pInhibit
+
+# Response Bias #
+def RI(dataList,names):
+    pGo,tmp = dataStats(dataList,names,'Go')
+    tmp,pNoGoi = dataStats(dataList,names,'NoGo')
+    ri = -(1/2)*(stats.zscore(pGo))
+
+
+
+
+
--- mainAnalysis.py
+++ mainAnalysis.py
@@ -6,11 +6,18 @@ import matplotlib.pyplot as plt
 import dataFuncs
 
 #--- load data ---#
-fileNames = ['3rdDayRewardHabBL32.txt','3rdDayRewardHabBR22.txt','3rdDayRewardHabTL31.txt','3rdDayRewardHabTR24.txt']
-names = ['BL32','BR22','TL31','TR24'] # Animal IDs
+fileNames = ['24thNOGOTrainingBL32.txt','24thNOGOTrainingBR22.txt']
+names = ['BL32','BR22'] # Animal IDs
 dataList = dataFuncs.loadData(fileNames) # creates list of complete datasets | dataList[animal][row][col]
 
 #--- get reward info per animal ---#
 rewardCounts = dataFuncs.getRewardCounts(dataList,names) # returns / prints the percent performance | (number of trials with reward / total number of trials) * 100
 
+#--- get Go performance ---#
+goCorrect,goIncorrect = dataFuncs.dataStats(dataList,names,'Go')
 
+#--- get No Go performance ---#
+nogoCorrect,nogoIncorrect = dataFuncs.dataStats(dataList,names,'NoGo')
+
+#--- Calculate pInhibition ---#
+pInhibition = dataFuncs.pInhibition(dataList,names)
